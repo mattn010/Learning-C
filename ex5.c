0a2d6195c0d055f42360fcf9c5d94e2525053e1c
mattn010 <mln24@bath.ac.uk>
2014-10-15 14:57:37 +0100

    ex7

---
+++ ex5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/* LOOK A COMMENT */
+
+int main(int argc, char *argv[])
+{
+	int distance = 100;
+
+	printf("Apparently you are %d miles away.\n", distance);
+
+	return 0;
+}
