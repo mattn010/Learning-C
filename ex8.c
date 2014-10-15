6473279b68389e27d56b41db01e752644e204f9c
mattn010 <mln24@bath.ac.uk>
2014-10-15 19:06:25 +0100

    ex9

--- ex8.c
+++ ex8.c
@@ -4,16 +4,20 @@ int main(int argc, char *argv[])
 {
 	int areas[] = {10,12,13,14,20};
 	char name[] = "Zed";
-	char full_name = {
-		'Z', 'e', 'd',
-		' ', 'A', '.', ' ',
-		'S', 'h', 'a', 'w', '\0'
-	};
+	char full_name[] = { 'Z', 'e', 'd', ' ', 'A', '.', ' ', 'S', 'h', 'a', 'w', '\0' };
 
 	//WARNING: Some systems require %u instead of % ld since it will use unsigned ints
 
 	printf("The size of an int: %ld\n", sizeof(int));
-	printf("The size of areas")
+	printf("The size of areas (int[]): %ld\n", sizeof(areas));
+	printf("The number of ints in areas: %ld\n", sizeof(areas) / sizeof(int));
+	printf("The first area is %d and the second area is %d\n", areas[0], areas[1]);
+	printf("The size of a char: %ld\n", sizeof(char) );
+	printf("The size of a name (char[]): %ld\n", sizeof(name));
+	printf("The number of chars: %ld\n", sizeof(name) / sizeof(char) );
+	printf("The size of full_name (char[]): %ld\n", sizeof(full_name));
+	printf("The number of chars: %ld\n", sizeof(full_name) / sizeof(char));
+	printf("name=\"%s\" and full_name=\"%s\"\n", name, full_name );
 
-	}
+	return 0;
 }
